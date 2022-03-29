f70b89aaa199e63f75f0232915b295dc67f4eec9
mateo.rodriguez <mateo.rodriguez@1MRODRIGZX5520.babel-si.local>
2022-03-30 01:58:18 +0200

    ejercicio9 resuelto

---
+++ Ejercicio9/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ejercicio9';
+  nombre = 'Mateo Rodriguez Santander';
+  telefono = 'Teléfono: 662603700';
+  email = "mateorodriguezsantander@gmail.com";
+  linkedin = "linkedin.com/mateorsantander"
+  titulacion = "Ingeniería de Software(2016-Act.)";
+  universidad = "Universidad Politécnica de Madrid";
+  nota = "Nota media: Notable (7)";
+  especialidad = "Especialización: Transformación digital y evolución software";
+}
